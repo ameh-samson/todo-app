16eb94725110a9da53665f3f0d4c3a5d1c2a98ff
Samson Ameh <amehsam69@gmail.com>
2024-02-06 22:51:20 +0100

    add general style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Todo from "./components/Todo/Todo";
-
+import "./App.css";
 function App() {
   return (
     <div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import styles from "./Header.module.css";
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <h1>My Todo List</h1>
+    </div>
+  );
 };
 
 export default Header;
