99812a5c7903f4bccb9481f7f62d4017984c571c
Samson Ameh <amehsam69@gmail.com>
2024-02-07 20:17:05 +0100

    renders todos conditionally

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,14 @@
 import TodoItem from "../TodoItem/TodoItem";
 import styles from "./TodoList.module.css";
 const TodoList = ({ todos }) => {
-  return (
+  return todos.length > 0 ? (
     <div className={styles.todoList}>
       {todos.map((item) => (
         <TodoItem item={item} key={item} />
       ))}
     </div>
+  ) : (
+    <p className={styles.todoList}>Nothing here yet...</p>
   );
 };
 
