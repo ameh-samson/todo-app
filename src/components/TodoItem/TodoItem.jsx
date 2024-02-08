32cd101a6d3bea32f6e746e132b589d05738841e
Samson Ameh <amehsam69@gmail.com>
2024-02-08 14:47:36 +0100

    add complete line through

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -4,10 +4,21 @@ const TodoItem = ({ item, todos, setTodos }) => {
   const handleDelete = (item) => {
     setTodos(todos.filter((todo) => todo !== item));
   };
+
+  const handleClick = (name) => {
+    const newArr = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : ""
+    );
+    setTodos(newArr);
+  };
+
+  const itemDone = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={itemDone} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}
