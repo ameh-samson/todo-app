c19720711bfec8d48c2343301a6f2944fb32bdd7
Samson Ameh <amehsam69@gmail.com>
2024-02-08 11:26:18 +0100

    adds delete button

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -3,7 +3,13 @@ import styles from "./TodoItem.module.css";
 const TodoItem = ({ item }) => {
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button className={styles.deleteBtn}>x</button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
