75a55bef4144f428317e514fb5c480a1712481dd
Samson Ameh <amehsam69@gmail.com>
2024-02-08 18:16:31 +0100

    add media query

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import styles from "./Footer.module.css";
 const Footer = ({ completedTodos, totalTodos }) => {
   return (
     <footer className={styles.footer}>
-      <span className={styles.item}>Completed Todos: {completedTodos}</span>
+      <span className={styles.item}>Completed Todos: {completedTodos}</span>{" "}
+      <br />
       <span className={styles.item}>Completed Todos: {totalTodos}</span>
     </footer>
   );
