494b0916c59184953ec5b7715669e12d30c87b73
Samson Ameh <amehsam69@gmail.com>
2024-02-08 18:26:44 +0100

    add padding

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,6 @@ const Footer = ({ completedTodos, totalTodos }) => {
   return (
     <footer className={styles.footer}>
       <span className={styles.item}>Completed Todos: {completedTodos}</span>{" "}
-      <br />
       <span className={styles.item}>Completed Todos: {totalTodos}</span>
     </footer>
   );
